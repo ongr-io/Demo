6b7b8dace9fe38a201821c96c81504999987337a
Mantas <marc.mantas@gmail.com>
2016-05-03 12:42:10 +0300

    made products display with images and variants

--- app/Resources/scripts/pages/product.js
+++ app/Resources/scripts/pages/product.js
@@ -14,99 +14,19 @@
     var Product = function() {
         var locale = appLocale;
         var selectors = {
-            productGallery:      '.product-gallery',
-            smoothProducts:      '.product-gallery .product-media',
-            productId:           '.product-variants .product-id',
-            variants:            '.product-variants .product-variant',
-            colorSelector:       '.product-color-name',
-            materialSelector:    '.product-material-name'
+            images:      '.image-in-slider',
+            mainImage:   '.product-image'
         };
 
-        var colorSelected, materialSelected;
-        var loader = new Ongr.Components.Loader(selectors.productGallery);
-
-        var bindSmoothProducts = function(smoothProducts) {
-            $(smoothProducts).smoothproducts();
-        };
-
-        var focusVariant = function(obj) {
-            $(obj).siblings(selectors.variants).removeClass('active');
-            $(obj).addClass('active');
-        };
-
-        var bindProductVariantSelected = function(variants) {
-            var product = new Ongr.Services.Product();
-            var productId = $(selectors.productId).text();
-            $(variants).each(function(i, variant) {
-                $(variant).unbind('click');
-                $(variant).bind('click', function() {
-                    loader.start();
-                    focusVariant(this);
-                    bookmarkVariantSelected(this);
-                    product.getVariants(productId).then(loadVariant);
-                });
+        var bindImageSlider = function(images, mainImage) {
+            $(images).on('click', function(){
+                $(mainImage).attr('src', $(this).attr('src'));
             });
-        };
-
-        var bookmarkVariantSelected = function(obj) {
-            var temp = $(obj).find(selectors.colorSelector).text();
-            colorSelected = (temp != '') ? temp: colorSelected;
-            temp = $(obj).find(selectors.materialSelector).text();
-            materialSelected = (temp != '') ? temp: materialSelected;
-        };
-
-        var checkVariantSelected = function(variants) {
-            if (!colorSelected) {
-                colorSelected = variants[0]['color'][locale]['text'];
-            }
-            if (!materialSelected) {
-                materialSelected = variants[0]['material'][locale]['text'];
-            }
-        };
-
-        var mapVariant = function(variant) {
-            if(colorSelected != variant['color']) {
-                return false;
-            } else if (materialSelected != variant['material']) {
-                return false;
-            }
-
-            return true;
-        };
-
-        var loadVariantImages = function(images) {
-            $(selectors.smoothProducts).empty();
-            $.each(images, function(i, image) {
-                var link = $('<a>'), img = $('<img>');
-                link.attr('href', image['thumbnail']);
-                img.attr('src', image['image']);
-                link.append(img);
-                $(selectors.smoothProducts).append(link);
-            });
-
-            bindSmoothProducts(selectors.smoothProducts);
-        };
-
-        var loadVariant = function(variants) {
-            if($.isArray(variants)) {
-                checkVariantSelected(variants);
-                $.each(variants, function(i, variant) {
-                    if(mapVariant({
-                        'color': variant['color'][locale]['text'],
-                        'material': variant['material'][locale]['text']
-                    })) {
-                        loadVariantImages(variant['images']);
-                        return false;
-                    }
-                })
-            }
-            loader.stop();
         };
 
         return {
             init: function() {
-                bindSmoothProducts(selectors.smoothProducts);
-                bindProductVariantSelected(selectors.variants);
+                bindImageSlider(selectors.images, selectors.mainImage);
             }
         };
     };
